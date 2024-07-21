bf09f3f3eb8482e093eba9ece55e91da9c460881
Salomon <esteban.vivanco.progra@gmail.com>
2024-07-21 17:51:18 -0400

    CORS

--- django_api_funebrin/settings.py
+++ django_api_funebrin/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
 REST_FRAMEWORK = {
     # YOUR SETTINGS
